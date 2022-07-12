c52dc4ca4ed00cee1fe344324705f37d7df895cf
samridht23 <samridht23@gmail.com>
2022-07-12 13:57:36 +0530

    body added

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,9 +1,19 @@
-import { Box } from "@chakra-ui/react";
-import axios, { Axios } from "axios";
+import { Box, Flex, Image, useColorModeValue } from "@chakra-ui/react";
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 const baseurl =
-  "https://api.unsplash.com/photos/?client_id=QiEe2pR0aGPwvYAkj8wOolp3LJOCggwxETxwuwb1rcg";
+  "https://api.unsplsh.com/photos/?client_id=QiEe2pR0aGPwvYAkj8wOolp3LJOCggwxETxwuwb1rcg";
+
+const data = {
+  isNew: true,
+  imageURL:
+    "https://images.unsplash.com/photo-1657564793579-9d49d4d7257b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  name: "Wayfarer Classic",
+  price: 4.5,
+  rating: 4.2,
+  numReviews: 34,
+};
 const Body = () => {
   const [data, setData] = useState([]);
 
@@ -27,8 +37,27 @@ const Body = () => {
   }, []);
   return (
     <div>
-      <Box></Box>
+      <Box bg={"white"} lineHeight={0} columnGap={"0px"} columnCount={5}>
+        <DesktopCard />
+        <DesktopCard />
+        <DesktopCard />
+        <DesktopCard />
+      </Box>
     </div>
   );
 };
+const DesktopCard = () => {
+  return (
+    <Flex p={5} alignItems="center" justifyContent="center">
+      <Image
+        w={"100%"}
+        h={"auto"}
+        src={data.imageURL}
+        alt={`Picture of ${data.name}`}
+        rounded="sm"
+      />
+    </Flex>
+  );
+};
+
 export default Body;
